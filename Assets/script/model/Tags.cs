32647404fcacd6090c02aacc21324e512731ae36
FENG-MASTER <1413161501@qq.com>
2017-01-12 00:28:50 +0800

    更新了下tags, 帮你们取好名字了,(笑

--- Assets/script/model/Tags.cs
+++ Assets/script/model/Tags.cs
@@ -10,5 +10,16 @@ public class Tags {
 
     public const string RAILWAY = "railway";
     public  const string RAILWAY_MOVABLE = "railway_movable";
+    public const string ROAD = "road";
+    public const string ROAD = "road";
+
+
+    public static class Vehicle {
+        public const string BASETRAMCAR = "base_tramcar";//基础矿车
+        public const string OVERWEIGHTTRAMCAR = "overweight_ramcar";//超载矿车
+        public const string SKILLEDTRAMCAR = "skilled_ramcar";//老练矿车
+        public const string TRAIN = "train";//火车
+        
+    }
 
 }
