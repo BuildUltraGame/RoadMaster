1659252d3a866b5c132ac6be7fecddd5ad10d86a
uncleduo <yiduo_wang@163.com>
2017-01-21 16:47:17 +0800

    改下文件名，防止跟伍峰冲突

---
+++ Assets/script/model/entity/duo_Building/ScoringBuilding.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 终点类
+/// 
+/// 
+/// </summary>
+public class ScoringBuilding : MonoBehaviour {
+
+    
+
+
+	// Use this for initialization
+	void Start () {
+        ScoreBoard.getInstance().Init(2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        //此处违反了之前规定的写对对方造成的影响...但我觉得...这样写舒服...
+        GameobjBase gBase= other.GetComponent<GameobjBase>();
+        GoldCarrier carrier=other.GetComponent<GoldCarrier>();
+
+        if(gBase==null||carrier==null){
+            return;
+        }
+
+        ScoreBoard.getInstance().addScore(gBase.getOwner(),carrier.popGold());
+
+        Destroy(other.gameObject);
+        
+    }
+
+
+
+    
+}
