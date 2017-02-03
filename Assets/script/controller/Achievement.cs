b3c0d91d9c5b5fc5f73896b2b0e6d7448866794b
JimyTD <604384365@qq.com>
2017-02-03 19:59:35 +0800

    controller 2.3

---
+++ Assets/script/controller/Achievement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+struct ACHIEVEMENT_WORD//保存成就描述的结构体
+{
+    public string name;
+    public string description;
+}
+//管理成就系统的类
+[System.Serializable]
+public class Achievement : MonoBehaviour {
+    int amount;
+
+    public void enable(int num)
+    {
+        achievement[num] = 1;   
+    }
+
+    int[] achievement;
+
+    Achievement()
+    {
+        amount = 1;///////成就数量
+        achievement = new int[amount];
+        for (int i = 0; i < amount; i++)
+        {
+            achievement[i] = 0;
+        }
+    }
+    // Use this for initialization
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/script/controller/gameStatus.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+//管理游戏状态的类
+[System.Serializable]
+public class savedData
+{
+    public Achievement achievement;
+    public Progress progress;
+}
+
+public class gameStatus : MonoBehaviour {
+
+    //管理基本的游戏状态
+
+
+    //全局属性
+    int Mode;
+    savedData data=new savedData();
+
+    //method
+    int signIn() {
+        return 0;
+    }
+
+    void setMode(int mode)
+    {
+        Mode = mode;
+    }
+
+    void save()
+    {
+        int flag = 0;
+        string path=Application.persistentDataPath;
+        path = path + @"save.dat";
+        if (File.Exists(path))//已经存在存档
+        {
+            flag = 1;
+        }
+        if (flag == 1)
+        {
+            FileStream stream = File.OpenRead(path);
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(stream, data);
+            stream.Flush();
+            stream.Close();
+        }
+        else
+        {
+            FileStream stream = File.Create(path);
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(stream, data);
+            stream.Flush();
+            stream.Close();
+        }
+    }
+
+    void saveAuto ()
+    {
+        save();
+    }
+
+    void load()
+    {
+        int flag = 0;
+        string path = Application.persistentDataPath;
+        path = path + @"save.dat";
+        if (File.Exists(path))//已经存在档位
+        {
+            flag = 1;
+        }
+        if (flag == 1)
+        {
+            FileStream stream = File.OpenRead(path);
+            BinaryFormatter formatter = new BinaryFormatter();
+            data = (savedData)formatter.Deserialize(stream);
+            stream.Close();
+        }
+        else
+        {
+            FileStream stream = File.Create(path);
+            BinaryFormatter formatter = new BinaryFormatter();
+            data = (savedData)formatter.Deserialize(stream);
+            stream.Close();
+        }
+    }
+    // Use this for initialization
+	void Start () {
+        setMode(constDic.INITIAL);//初始界面
+        load();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
