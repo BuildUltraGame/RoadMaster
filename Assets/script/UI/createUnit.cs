9a4b87a1b77429926447fcd61c024854703d62f7
JimyTD <604384365@qq.com>
2017-03-02 23:00:23 +0800

    子昀 更新3.2

---
+++ Assets/script/UI/createUnit.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class createUnit : MonoBehaviour {
+    public MineMountain mm;
+    private UISprite producer;
+    public List<int> nameList;
+    public List<string> picList;
+    private int nameNum;
+    public Vector3 pos=Vector3.zero;
+    public Reminder rm;
+
+    public Dictionary<int, string> nameDict = new Dictionary<int, string>();
+    public TextAsset nameInfoText;
+
+
+    void Awake()
+    {
+        InitText();
+    }
+    void Start()
+    {
+        producer = this.gameObject.GetComponent<UISprite>();
+
+        foreach (KeyValuePair<int, string> item in nameDict)
+        {
+            for (nameNum = 0; nameNum < 4; nameNum++)
+            {
+                picList[nameNum]=nameDict[nameList[nameNum]];
+            }
+        }
+        nameNum = 0;
+        producer.spriteName = picList[nameNum];
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    void InitText()
+    {
+        string text = nameInfoText.text;
+        string[] strArray = text.Split('\n');
+        foreach (string str in strArray)
+        {
+            string[] proArray = str.Split(',');
+            int id = int.Parse(proArray[0]);
+            string name = proArray[1];
+            nameDict.Add(id, name);
+        }
+    }
+    public void frontButton()
+    {
+        nameNum++;
+        if (nameNum >= 4)
+        {
+            nameNum = 0;
+        }
+
+        producer.spriteName = picList[nameNum];
+    }
+
+    public void nextButton()
+    {
+        nameNum--;
+        if (nameNum < 0)
+        {
+            nameNum = 3;
+        }
+        producer.spriteName = picList[nameNum];
+    }
+/// <summary>
+/// 未知
+/// </summary>
+/// <param name="newNameList"></param>
+    public void updateNameList(List<int> newNameList)
+    {
+        nameList = newNameList;
+    }
+    /// <summary>
+    /// 用户点击制造单位，未完成
+    /// </summary>
+    public void OnClick()
+    {
+        if (pos == Vector3.zero)
+        {
+            rm.sendHint("please choose your point");
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            
+        }
+        /*else 
+        { 
+            mm.buildUnitByName(picList[nameNum], pos);
+            pos = Vector3.zero;
+            Tags.RAILWAY
+        }*/
+    }
+}
---
+++ Assets/script/UI/mineGold.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mineGold : MonoBehaviour {
+    public Reminder remind;
+    public UILabel goldNow;
+    public UILabel goldNeed;
+    public UILabel GoldAll;
+    public UILabel Train;
+    public UILabel GoldGet;
+    public UILabel GoldTime;
+    MineMountain mineSelected;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void changeGold0()
+    {
+        goldNow.text = "";
+    }
+    public void changeGold1()
+    {
+        goldNeed.text = "";
+    }
+    public void changeGold2()
+    {
+        if (mineSelected == null)
+           remind.sendHint("please select a mine");
+        else GoldAll.text = ""+mineSelected.totalMine;
+    }
+    public void changeGold3()
+    {
+        Train.text = "";
+    }
+    public void changeGold4()
+    {
+        GoldGet.text = "";
+    }
+    public void changeGold5()
+    {
+        GoldTime.text = "";
+    }
+}
