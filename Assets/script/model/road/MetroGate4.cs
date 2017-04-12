7c16541a8ac79e0d23d0bbaf5f835824eed6d809
FENG-MASTER <1413161501@qq.com>
2017-04-12 21:46:20 +0800

    所有车载单位重量增加,这样惯性就会减少,还有增加了道闸口情况的显示

--- Assets/script/model/road/MetroGate4.cs
+++ Assets/script/model/road/MetroGate4.cs
@@ -8,10 +8,12 @@ public class MetroGate4 : MetroGate
     private int state = 0;
 
 
+
     void Start()
     {
         base.Start();
         initPointInfo();
+        GetComponent<Renderer>().material.mainTexture = stateTexture[state];
 
     }
 
@@ -36,23 +38,21 @@ public class MetroGate4 : MetroGate
   
         if (state==0)
         {
-            //如果工人来的位置原来就没有连接,那就是情况2
-            //随机将所在点和其他两个点联通
+           
             link.startTransform = allPoint[0];
             link.endTransform = allPoint[2];
             state = 1;
         }
         else if(state==1)
         {
-            //如果工人来的位置原来已经和某铁路连接,那就是情况1
-            //直接转换另一个接口到未连接接口
+            
             link.startTransform = allPoint[1];
             link.endTransform = allPoint[3];
 
             state = 0;
         }
 
-
+        GetComponent<Renderer>().material.mainTexture = stateTexture[state];
   
         destroyVehilesOnRoad();//摧毁当前在岔道口的车辆
     }
