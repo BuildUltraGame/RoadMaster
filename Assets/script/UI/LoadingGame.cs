f53b6dfa18c3f99d1087b285038eea589a712514
uncleduo <yiduo_wang@163.com>
2017-04-30 11:14:20 +0800

    增加了异步加载的功能，具体用法回去一起看看吧

---
+++ Assets/script/UI/LoadingGame.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// 异步加载场景脚本
+/// 使用方法：先设置Globe.LOADINGSCENCE，然后加载场景LoadingGame
+/// </summary>
+public class LoadingGame : MonoBehaviour {
+
+    AsyncOperation async;
+    private float progress = 0;//场景加载到多少了
+    public UIScrollBar scrollBar;
+
+    void Start()
+    {
+        StartCoroutine(loadScene());
+    }
+
+    IEnumerator loadScene()
+    {
+        async = SceneManager.LoadSceneAsync("wf");
+        yield return async;
+    }
+
+    void Update()
+    {
+        scrollBar.value = progress;
+    }
+
+
+}
