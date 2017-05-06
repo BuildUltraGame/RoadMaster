2b72088426ab56520692df73a1dbb4fb00d9a392
FENG-MASTER <1413161501@qq.com>
2017-05-06 13:56:05 +0800

    ..

---
+++ Assets/script/controller/AudioManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEvent;
+/// <summary>
+/// 语音控制器
+/// </summary>
+public class AudioManager : MonoBehaviour,IListener<AudioEvent> {
+    public static AudioManager instance;
+
+    private Queue<AudioEvent> audios = new Queue<AudioEvent>();
+
+    private AudioEvent audioNow;
+
+	void Awake() {
+        instance = this;
+        UnityEventCenter.Register<AudioEvent>(this);
+    }
+
+    public static AudioManager getInstance()
+    {
+        return instance;
+    }
+	
+	void Update () {
+        if (audioNow==null)
+        {
+
+            do
+            {
+                audioNow = audios.Dequeue();
+
+            } while (!audioNow.isAble());
+            
+            audioNow.getAudio().Play();
+        }
+        else
+        {
+            if (!audioNow.getAudio().isPlaying)
+            {
+                audioNow = null;
+            }
+        }
+        
+	}
+
+    public void Handle(AudioEvent message)
+    {
+        audios.Enqueue(message);
+    }
+}
---
+++ Assets/script/controller/event/AudioEvent.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 播放语音事件
+/// </summary>
+public class AudioEvent : BaseEvent {
+    private AudioSource audio;
+    private float maxWait = 0;
+    private float sendTime = 0;
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="obj">游戏物体</param>
+    /// <param name="audio">语音</param>
+    /// <param name="maxWait">语音播放最大等待时间</param>
+    public AudioEvent(GameObject obj, AudioSource audio,float maxWait):base(obj,"playAudio",null)
+    {
+        this.audio = audio;
+        this.maxWait = maxWait;
+        sendTime=Time.realtimeSinceStartup;
+    }
+
+    public AudioSource getAudio()
+    {
+        return audio;
+    }
+
+    public bool isAble()
+    {
+        if (Time.realtimeSinceStartup-sendTime<maxWait)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+}
