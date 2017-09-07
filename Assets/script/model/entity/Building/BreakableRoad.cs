9caba9ec9a7d157919cac95365152ad24a761cba
FENG-MASTER <1413161501@qq.com>
2017-09-07 15:49:05 +0800

    补提交

--- Assets/script/model/entity/Building/BreakableRoad.cs
+++ Assets/script/model/entity/Building/BreakableRoad.cs
@@ -10,19 +10,32 @@ public class BreakableRoad : MonoBehaviour {
     public RoadPoint p1;
     public RoadPoint p2;
 
+    private float repairTime = 1f;
+    private float breakTime = 1f;
+
+
+    public Animator animator;
+
+
     private bool breakFlag = true;//路是否破坏标志位
 
     /// <summary>
-    /// 修路接口
+    /// 修路接口,修理需要时间 repairTime
     /// </summary>
     public void repairRoad()
     {
+      
         if (breakFlag)
         {
-            p1.reachList.Add(p2);
-            p2.reachList.Add(p1);
+            playRepairRoad();
+            Invoke("repairRoadNow", repairTime);
         }
+    }
 
+    private void repairRoadNow()
+    {
+        p1.reachList.Add(p2);
+        p2.reachList.Add(p1);
     }
 
 
@@ -31,11 +44,34 @@ public class BreakableRoad : MonoBehaviour {
     /// </summary>
     public void breakRoad()
     {
+       
         if (!breakFlag)
         {
-            p2.reachList.Remove(p1);
-            p1.reachList.Remove(p2);
+            playBreakRoad();
+            Invoke("breakRoadNow", breakTime);
         }
+
+    }
+    
+    private void breakRoadNow()
+    {
+        p2.reachList.Remove(p1);
+        p1.reachList.Remove(p2);
+    }
+
+    /// <summary>
+    /// 播放修路动画
+    /// </summary>
+    private void playRepairRoad()
+    {
+        animator.Play("build");
+    }
+
+    /// <summary>
+    /// 播放 破坏路动画
+    /// </summary>
+    private void playBreakRoad()
+    {
 
     }
 
