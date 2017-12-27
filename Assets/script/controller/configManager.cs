fbd05296815feaa81fa6b85d7b326e0c1a533b44
JimyTD <604384365@qq.com>
2017-12-27 10:28:26 +0800

    csv读取雏形

--- Assets/script/controller/configManager.cs
+++ Assets/script/controller/configManager.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 
 public class configManager : MonoBehaviour {
 
-	
+    string[] properties;//读取属性项
+    int propertiesAmount;//属性数量
+    Dictionary<int, Dictionary<string, string>> config = new Dictionary<int, Dictionary<string, string>>();//配置字典
     // Use this for initialization
-	void Start () {
+    void Start () {
 		
 	}
 	
@@ -15,4 +17,32 @@ public class configManager : MonoBehaviour {
 	void Update () {
 		
 	}
+    /// <summary>
+    /// 读取配置
+    /// </summary>
+    void readConfig()
+    {
+
+    }
+
+    /// <summary>
+    /// 使用id获取对应游戏对象的属性,返回一个string,如果属性为int 或者float可以强转
+    /// </summary>
+    /// <param name="id">游戏对象的id</param>
+    /// <param name="property">你需要查找的属性</param>
+    public string getInformationById(int id,string property)
+    {
+        return null;
+    }
+
+    /// <summary>
+    /// 通过已知的属性内容来获取对应的id
+    /// </summary>
+    /// <param name="property">属性的名字</param>
+    /// <param name="content">属性的值</param>
+    /// <returns></returns>
+    public int searchIdByInformation(string property,string content)
+    {
+        return 0;
+    }
 }
